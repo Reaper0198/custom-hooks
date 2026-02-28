49f77981a13b91cf208ac8ca6bb3945729b72610
kmTyagi198 <k.m.tyagi198@gmail.com>
2026-02-28 16:21:45 +0530

    added logic for advanced useFetch hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import GetPostCard from './components/getPostCard';
 import IncreaseValueCard from './components/IncreaseValueCard';
+import PollingGetPostCard from './components/PollingGetPostCard';
 
 export default function App() {
     return (
         <>
             <IncreaseValueCard />
             <GetPostCard />
+            <PollingGetPostCard />
         </>
     )
 }
---
+++ src/hooks/useFetch2.js
@@ -0,0 +1,43 @@
+// this is advance fetch it handles loading, error, fetched data
+
+import { useEffect, useState } from "react";
+
+export default function useFetch2(url, polTime) {
+
+    const [postData, setPostData ] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    const gettingData = async () => {
+
+        try{
+            if(!url) return 
+
+            setLoading(true);
+            setError(false);
+            const response = await fetch(url);
+            const data = await response.json();
+            setPostData(data);
+            setLoading(false);
+        }catch(e){
+            setError(true);
+            setLoading(false);
+            setPostData(null);
+        }
+
+    }
+
+    useEffect(() => {
+        gettingData();
+        const intervalId = setInterval(gettingData, polTime * 1000);
+
+        return () => clearInterval(intervalId);
+    }, [url, polTime]);
+
+    return {
+        loading,
+        error,
+        postData
+    }
+
+}   
