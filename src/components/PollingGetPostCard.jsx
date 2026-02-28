8cf07d60aa63cfa78e163b586ad265c20fa56a29
kmTyagi198 <k.m.tyagi198@gmail.com>
2026-02-28 16:22:20 +0530

    created component for useFetch2

---
+++ src/components/PollingGetPostCard.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import useFetch2 from "../hooks/useFetch2";
+
+export default function PollingGetPostCard() {
+
+    const [url, setUrl] = useState(null);
+
+    const {loading, postData, error} = useFetch2(url, 5);
+
+    console.log('loading', loading)
+    console.log('postData', postData)
+    console.log('error', error)
+
+    const getPost = (postId) =>{
+        setUrl(`https://jsonplaceholder.typicode.com/posts/${postId}`);
+    }
+
+    return (
+        <div className="flex-col border-2 m-4 p-2 rounded-lg">
+            <div className="flex gap-2">
+                <button className="p-1 border-2 border-black rounded-lg bg-green-300 text-white" 
+                    onClick={()=>getPost(1)}>click me fetch post 1</button>
+                <button className="p-1 border-2 border-black rounded-lg bg-green-300 text-white" 
+                    onClick={()=>getPost(2)}>click me fetch post 2</button>
+                <button className="p-1 border-2 border-black rounded-lg bg-green-300 text-white" 
+                    onClick={()=>getPost(3)}>click me fetch post 3</button>
+            </div>
+            <div>
+                {loading && <p>Loading...</p>}
+                {error && <p>Some error occured...</p>}
+                {postData && <p>{JSON.stringify(postData)}</p>}
+            </div>
+
+        </div>
+    )
+}
